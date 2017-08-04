9f49fbc506e3a826af6a46e566f85de398a56ba3
root <root@MOONS.lan>
2017-08-04 10:50:13 +0800

    initial Agenda system

---
+++ include/Path.hpp
@@ -0,0 +1,21 @@
+#ifndef PATH_HPP
+#define PATH_HPP
+
+class Path {
+public:
+    /**
+     * user.csv path
+     */
+    static const char *userPath;
+
+    /**
+     * meeting.csv path
+     */
+    static const char *meetingPath;
+};
+
+const char *Path::meetingPath = "meetings.csv";
+const char *Path::userPath = "users.csv";
+
+
+#endif
