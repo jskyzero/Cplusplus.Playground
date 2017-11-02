df7c5cccbce4b5b3a7af0332781fb8ceafae53c4
jskyzero <jskyzero@outlook.com>
2017-11-02 12:28:42 +0800

    update c98 reference

--- hardwork/C++98/reference.cpp
+++ hardwork/C++98/reference.cpp
@@ -1,7 +1,63 @@
 #include <iostream>
 
+int& getIntReference(int** p, int n) {
+  int* num = new int;
+  *p = num;
+  std::cout << "memory address = " << *p << std::endl;
+  *num = n;
+  return *num;
+}
+
 int main() {
   int a = 1;
-  int & b = a;
+  int& b = a;
+
+  a = 2;
+  std::cout << "a = " << a << " "
+            << "&a = " << &a << std::endl
+            << "b = " << b << " "
+            << "&b = " << &b << std::endl
+            << std::endl;
+
+  int* p = NULL;
+  a = getIntReference(&p, -1);
+  std::cout << "a = " << a << " "
+            << "&a = " << &a << std::endl
+            << "b = " << b << " "
+            << "&b = " << &b << std::endl
+            << std::endl;
+  std::cout << "memory address = " << p << std::endl;
+  *p = 0;
+  std::cout << "a = " << a << " "
+            << "&a = " << &a << std::endl
+            << "b = " << b << " "
+            << "&b = " << &b << std::endl
+            << std::endl;
+  delete p;
+  std::cout << "a = " << a << " "
+            << "&a = " << &a << std::endl
+            << "b = " << b << " "
+            << "&b = " << &b << std::endl
+            << std::endl;
+
+  b = getIntReference(&p, -2);
+  std::cout << "a = " << a << " "
+            << "&a = " << &a << std::endl
+            << "b = " << b << " "
+            << "&b = " << &b << std::endl
+            << std::endl;
+  std::cout << "memory address = " << p << std::endl;
+  *p = 0;
+  std::cout << "a = " << a << " "
+            << "&a = " << &a << std::endl
+            << "b = " << b << " "
+            << "&b = " << &b << std::endl
+            << std::endl;
+  delete p;
+  std::cout << "a = " << a << " "
+            << "&a = " << &a << std::endl
+            << "b = " << b << " "
+            << "&b = " << &b << std::endl
+            << std::endl;
   return 0;
 }
