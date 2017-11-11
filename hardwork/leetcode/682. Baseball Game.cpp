2b91f46b8cbce3453debfd7b91438cfb156f4beb
jskyzero <jskyzero@outlook.com>
2017-11-11 13:32:01 +0800

    finished leetcode 682

--- hardwork/leetcode/682. Baseball Game.cpp
+++ hardwork/leetcode/682. Baseball Game.cpp
@@ -7,15 +7,15 @@
 
 // universal header file
 #include <algorithm>
-#include <cstdlib>
-#include <ciso646>
 #include <cassert>
+#include <ciso646>
+#include <cstdlib>
 #include <iostream>
 #include <memory>
 #include <numeric>
-#include <vector>
-#include <stack>
 #include <set>
+#include <stack>
+#include <vector>
 
 using namespace std;
 
@@ -25,24 +25,31 @@ using namespace std;
 #endif
 
 class Solution {
-public:
-    int calPoints(vector<string>& ops) {
-        auto ans = stack<int>();
-        for (auto op: ops) {
-          if (op == "C") {
-            ans.pop();
-          } else if (op == "D") {
-            ans.push(ans.top * 2);
-          } else if (op == "+") {
-            auto last1 = ans.top();
-            ans.pop();
-            auto last2 = ans.top();
-            ans.push(last1);
-            ans.push(last2 + last1);
-          } else {
-            ans.push(atoi(op.c_str()));
-          }
-        }
-
+ public:
+  int calPoints(vector<string>& ops) {
+    auto ans = stack<int>();
+    for (auto op : ops) {
+      if (op == "C") {
+        ans.pop();
+      } else if (op == "D") {
+        ans.push(ans.top() * 2);
+      } else if (op == "+") {
+        auto last1 = ans.top();
+        ans.pop();
+        auto last2 = ans.top();
+        ans.push(last1);
+        ans.push(last2 + last1);
+      } else {
+        ans.push(atoi(op.c_str()));
+      }
     }
+    // return accumulate(ans.begin(), ans.end(), 0);
+    int sum = 0;
+    while(not ans.empty()) {
+        sum += ans.top();
+        ans.pop();
+    }
+    return sum;
+    }
+  }
 };
