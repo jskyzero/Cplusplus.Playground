622dc2ca077ce553c53920a74a7005bd2d2a7075
jskyzero <jskyzero@outlook.com>
2017-12-15 15:47:49 +0800

    add test img and image input

--- hardway/2.Huffman/Huffman.cpp
+++ hardway/2.Huffman/Huffman.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
+#include <opencv2/opencv.hpp>
 
+using namespace cv;
 using namespace std;
 
-#define MESSAGE "HelloWordl!"
+#define CAT_FILE_PARH "img/cat.png"
 
 int main() {
-  cout << MESSAGE << endl;
+  auto cat_img = imread(CAT_FILE_PARH, IMREAD_COLOR);
+  cout << cat_img.size() << endl;
+
   return 0;
 }
