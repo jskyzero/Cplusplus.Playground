7813a310ba2a4d027c4eeea405621a1a76326c95
jskyzero <jskyzero@outlook.com>
2018-02-14 14:09:54 +0800

    update tips/sizeof.c test

--- hardwork/tips/sizeof.c
+++ hardwork/tips/sizeof.c
@@ -2,16 +2,20 @@
 
 this code can be compiled both in C and C++
 
-I just want to test the sizeof :)  
+I just want to test the sizeof :)
 
 */
 #include <stdio.h>
 
 int main() {
-	int a[10];
-	printf("%d",  (sizeof(a) == sizeof(int) * 10));
-	int* p = a;
-	printf("%d\n", (sizeof(p) == sizeof(&a)));
-	
-	return 0;
+  int a[10];
+  int* p = a;
+  printf("int a[10];\n");
+  printf("int* p = a;\n");
+  printf("sizeof(a) = %lu\n", sizeof(a));
+  printf("sizeof(p) = %lu\n", sizeof(p));
+  printf("sizeof(&a) = %lu\n", sizeof(&a));
+  printf("sizeof(&a[0]) = %lu\n", sizeof(&a[0]));
+
+  return 0;
 }
